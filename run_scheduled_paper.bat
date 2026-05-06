90a217bfb8f9b50f05ae299e7117940e5aaf47fc
Jure Kolar <jure.kolar@gmail.com>
2026-05-06 21:07:17 +0200

    changes to the journal

--- run_scheduled_paper.bat
+++ run_scheduled_paper.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d C:\Users\jurek.MARUSA-PC\code\microcapital-trader
-.\.venv\Scripts\python.exe .\microcapital_trader.py --mode scheduled_paper >> paper_trading.log 2>&1
+.\.venv\Scripts\python.exe microcapital_trader.py --mode scheduled_paper >> paper_trading.log 2>&1
